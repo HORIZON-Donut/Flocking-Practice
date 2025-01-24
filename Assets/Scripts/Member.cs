53713fc5e65e116fec45a4bc313b331921b2acb1
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-24 16:09:10 +0700

    Add another stuff to member script agian

--- Assets/Scripts/Member.cs
+++ Assets/Scripts/Member.cs
@@ -17,4 +17,24 @@ public class Member : MonoBehaviour
 		position = transform.position;
 		celovity = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
 	}
+
+	void WrapAround(ref Vector3 vector, float min, float max)
+	{
+		vector.x = WrapAroundFloat(vector.x, min, max);
+		vector.y = WrapAroundFloat(vector.y, min, max);
+		vector.z = WrapAroundFloat(vector.z, min, max);
+	}
+
+	float WrapAroundFloat(float value, float min, float max)
+	{
+		if (value > max) {value = min;}
+		else if (value < min) {value = max}
+
+		return value;
+	}
+
+	float RandomBinomail()
+	{
+		return Random.Range(0f, 1f) - Random.Range(0f, 1f);
+	}
 }
