c1fc6623f6d55b2b4680c314569f93a71e08d1fc
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-24 15:14:16 +0700

    Add stuff to level script as setup stuff

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -2,15 +2,33 @@ using UnityEngine;
 
 public class Level : MonoBehaviour
 {
+	public Transform memberPrefab;
+	public Transform enemyPrefab;
+
+	public int numberOfMembers;
+	public int numberOfEnemies;
+
+	public List<Member> members;
+	public List<Enemy> enemies;
+
+	public float bounds;
+	public float spawnRadius;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+		members = new List<Member>();
+		enemies = new List<Enemy>();
+
+		Spawn(memberPrefab, numberOfMembers);
+		Spawn(enemyPrefab, numberOfEnemies);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Spawn(Transform prefab, int count)
+	{
+		for(int i = 0; i < count; i++)
+		{
+			Instantiate(prefab, new Vector3(Random.Range(-spawnnRadius, spawnRadius), 0, Random.Range(-spawnnRadius, spawnRadius)), Quaterniom.identity);
+		}
+	}
 }
