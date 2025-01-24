43830cf454b4b68b5e68cb94d790af11ff5e85b8
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-24 16:02:04 +0700

    Add stuff 1 to member script

--- Assets/Scripts/Member.cs
+++ Assets/Scripts/Member.cs
@@ -2,4 +2,19 @@ using UnityEngine;
 
 public class Member : MonoBehaviour
 {
+	public Vector3 position;
+	public Vector3 celocity;
+	public Vector3 acceleration;
+
+	public Level level;
+	public MemberConfig conf;
+
+	void Start()
+	{
+		level = FindObjectOfType<Level>();
+		conf = FindObjectOfTpye<MemberConfig>();
+
+		position = transform.position;
+		celovity = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), 0);
+	}
 }
