252d6b3c6a02a2f3d60ca2b645d3bff442754cca
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-28 19:55:17 +0700

    Add comment

--- Assets/Scripts/MemberConfig.cs
+++ Assets/Scripts/MemberConfig.cs
@@ -6,20 +6,25 @@ public class MemberConfig : MonoBehaviour
 	public float maxAcceleration;
 	public float maxVelocity;
 
+	//Wander variable
 	public float wanderJitter;
 	public float wanderRadius;
 	public float wanderDistance;
 	public float wanderPriority;
 
+	//Cohesuin variable
 	public float cohesionRadius;
 	public float cohesionPriority;
 
+	//Alignment variable
 	public float alignmentRadius;
 	public float alignmentPriority;
 
+	//Separate variable
 	public float separationRadius;
 	public float separationPriority;
 
+	//Avoid variable
 	public float avoidanceRadius;
 	public float avoidancePriority;
 
