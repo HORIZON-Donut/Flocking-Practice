31ca8b9a52d818188f77aa4c24b428a503cef7b6
HORIZON-Donut <kiettisak.k2546@gmail.com>
2025-01-28 21:34:23 +0700

    Add stuff to enemy script

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : Member
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	override protected Vector3 Combine()
+	{
+		return conf.wadnerPriority * Wander();
+	}
 }
